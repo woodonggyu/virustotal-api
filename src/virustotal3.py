0263f1514b6d336e2a2ce4402bfd4c8543005b8b
woodonggyu <woodonggyu92@gmail.com>
2021-07-17 13:44:13 +0900

    chore: Change Directory

--- src/virustotal3.py
+++ src/virustotal3.py
@@ -2,10 +2,10 @@
 
 """Public API constraints and restrictions
 
-The Public API is limited to 500 requests per day and a rate of 4 requests per minute.
+The Public API is limited to 500 requests per day and a rate of 4 requests per minute.  # noqa
 The Public API must not be used in commercial products or services.
-The Public API must not be used in business workflows that do not contribute new files.
-You are not allowed to register multiple accounts to overcome the aforementioned limitations.
+The Public API must not be used in business workflows that do not contribute new files. # noqa
+You are not allowed to register multiple accounts to overcome the aforementioned limitations.   # noqa
 
 .. _reference:
     https://developers.virustotal.com/v3.0/reference#overview
